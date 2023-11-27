c146c4c35773f443f188da0352170b5fe482b74a
LoukasPap <loukas.ppd@gmail.com>
2023-11-27 18:15:08 +0200

    Add template response test

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from queries import getLogRecords
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    results = getLogRecords(3)
-    return {"msg": results}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    results = getLogRecords(5)
+    # print(results) # debug
+    return templates.TemplateResponse("index.html", {"request": request, "result":results})
