779d60514dd9d65cb79b9e159d598cd34b195664
LoukasPap <loukas.ppd@gmail.com>
2023-11-28 00:25:39 +0200

    Add skeleton of APIs

--- backend/main.py
+++ backend/main.py
@@ -15,3 +15,56 @@ async def root(request: Request):
     results = getLogRecords(5)
     # print(results) # debug
     return templates.TemplateResponse("index.html", {"request": request, "result":results})
+
+
+@app.get("/query1")
+async def query1(request: Request):
+    return {"query":"query"}
+
+@app.get("/query2")
+async def query2(request: Request):
+    return {"query":"query"}
+
+@app.get("/query3")
+async def query3(request: Request):
+    return {"query":"query"}
+
+@app.get("/query4")
+async def query4(request: Request):
+    return {"query":"query"}
+
+@app.get("/query5")
+async def query5(request: Request):
+    return {"query":"query"}
+
+@app.get("/query6")
+async def query6(request: Request):
+    return {"query":"query"}
+
+@app.get("/query7")
+async def query7(request: Request):
+    return {"query":"query"}
+
+@app.get("/query8")
+async def query8(request: Request):
+    return {"query":"query"}
+
+@app.get("/query9")
+async def query9(request: Request):
+    return {"query":"query"}
+
+@app.get("/query10")
+async def query10(request: Request):
+    return {"query":"query"}
+
+@app.get("/query11")
+async def query11(request: Request):
+    return {"query":"query"}
+
+@app.get("/query12")
+async def query12(request: Request):
+    return {"query":"query"}
+
+@app.get("/query13")
+async def query13(request: Request):
+    return {"query":"query"}
--- backend/queries.py
+++ backend/queries.py
@@ -10,3 +10,49 @@ def getLogRecords(n: int):
         , (str(n), ))
         print('Works')
         return db.cursor.fetchall()
+    
+
+def query1():
+    pass
+
+def query2():
+    pass
+
+def query3():
+    pass
+
+def query4():
+    pass
+
+def query5():
+    pass
+
+def query6():
+    pass
+
+def query7():
+    pass
+
+def query8():
+    pass
+
+def query9():
+    pass
+
+def query10():
+    pass
+
+def query11():
+    pass
+
+def query12():
+    pass
+
+def query13():
+    pass
+
+def logUserAction():
+    pass
+
+def insertLog():
+    pass
