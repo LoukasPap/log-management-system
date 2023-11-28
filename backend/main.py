d1de360b1357385615c823083acffd7e56044282
LoukasPap <loukas.ppd@gmail.com>
2023-11-28 03:07:08 +0200

    Add first five (5) queries

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
-from queries import getLogRecords
+from queries import *
 
 app = FastAPI()
 
@@ -12,30 +12,38 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    results = getLogRecords(5)
-    # print(results) # debug
+    results = get_log_records(5)
     return templates.TemplateResponse("index.html", {"request": request, "result":results})
 
 
 @app.get("/query1")
-async def query1(request: Request):
-    return {"query":"query"}
+async def query1(start_date: str, end_date: str):
+    results = lp_type_ranged(start_date, end_date)
+    return {"msg" : results}
+
 
 @app.get("/query2")
-async def query2(request: Request):
-    return {"query":"query"}
+async def query2(start_date: str, end_date: str, type: str):
+    results = lp_day_specified_type_ranged(start_date, end_date, type)
+    return {"msg" : results}
+
 
 @app.get("/query3")
-async def query3(request: Request):
-    return {"query":"query"}
+async def query3(day: str):
+    results = top_lp_src_day(day)
+    return {"msg" : results}
+
 
 @app.get("/query4")
-async def query4(request: Request):
-    return {"query":"query"}
+async def query4(start_date: str, end_date: str):
+    results = action_based_top5_blocks_ranged(start_date, end_date)
+    return {"msg" : results}
+
 
 @app.get("/query5")
 async def query5(request: Request):
-    return {"query":"query"}
+    results = referers_with_at_least_2_resources()
+    return {"msg" : results}
 
 @app.get("/query6")
 async def query6(request: Request):
--- backend/queries.py
+++ backend/queries.py
@@ -1,7 +1,8 @@
 from database import PgDatabase
+from datetime import datetime
 
 
-def getLogRecords(n: int):
+def get_log_records(n: int):
     with PgDatabase() as db:
         db.cursor.execute(
             """
@@ -10,22 +11,56 @@ def getLogRecords(n: int):
         , (str(n), ))
         print('Works')
         return db.cursor.fetchall()
-    
 
-def query1():
-    pass
+# query1
+# http://127.0.0.1:8001/query1?start_date=2005-10-20%2022:24:46&end_date=2009-12-20%2022:24:48
+def lp_type_ranged(start_date: str, end_date: str):
+    with PgDatabase() as db:
+        db.cursor.callproc('query1', [convert_to_timestamp(start_date), 
+                                      convert_to_timestamp(end_date)])
+        res = db.cursor.fetchall()
+        return res
 
-def query2():
-    pass
 
-def query3():
-    pass
+# query2
+# http://127.0.0.1:8001/query2?start_date=2002-10-20%2022:24:46&end_date=2013-11-09%2021:20:55&type=ACCESS
+def lp_day_specified_type_ranged(start_date: str, end_date: str, action_type: str):
+    with PgDatabase() as db:
+        db.cursor.callproc('query2', [convert_to_timestamp(start_date), 
+                                      convert_to_timestamp(end_date), 
+                                      action_type])
+        res = db.cursor.fetchall()
+        return res
 
-def query4():
-    pass
 
-def query5():
-    pass
+# query3
+# http://127.0.0.1:8001/query3?day=2005-06-29
+def top_lp_src_day(day: str):
+    with PgDatabase() as db:
+        db.cursor.callproc('query3', [day])
+        res = db.cursor.fetchall()
+        return res
+
+
+# query 4
+# http://127.0.0.1:8001/query4?start_date=2005-10-20%2022:24:46&end_date=2009-12-20%2022:24:48
+def action_based_top5_blocks_ranged(start_date: str, end_date: str):
+    with PgDatabase() as db:
+        db.cursor.callproc('query4', [convert_to_timestamp(start_date), 
+                                      convert_to_timestamp(end_date)])
+        res = db.cursor.fetchall()
+        return res
+    # TODO fix: run in ~27 secs
+
+
+# query 5
+# http://127.0.0.1:8001/query5
+def referers_with_at_least_2_resources():
+    with PgDatabase() as db:
+        db.cursor.callproc('query5')
+        res = db.cursor.fetchall()
+        return res
+
 
 def query6():
     pass
@@ -56,3 +91,8 @@ def logUserAction():
 
 def insertLog():
     pass
+
+
+def convert_to_timestamp(dt: str):
+    datetime_object = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
+    return str(datetime_object.strftime("%y%m%d %H%M%S"))
